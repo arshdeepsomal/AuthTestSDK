c14b81d693daed8e4328431484400416140c107c
Arshdeep Singh Somal <arshdeep.singh.somal@gmail.com>
2025-11-28 18:47:01 -0500

    Make AuthApi public

--- auth-sdk/src/main/java/com/devconsole/auth_sdk/auth/api/AuthApi.kt
+++ auth-sdk/src/main/java/com/devconsole/auth_sdk/auth/api/AuthApi.kt
@@ -4,7 +4,7 @@ import androidx.activity.result.ActivityResult
 import com.devconsole.auth_sdk.auth.model.AuthState
 import kotlinx.coroutines.flow.StateFlow
 
-internal interface AuthApi {
+interface AuthApi {
     fun login()
     fun register()
     fun handleIntentResult(result: ActivityResult)
